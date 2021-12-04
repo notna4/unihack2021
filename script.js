c195e9caad8d04ad2fd90d4cc6668ab3db14a82b
Cristian Canceal <cristicanceal@gmail.com>
2021-12-05 01:50:33 +0200

    cleaned up the code

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicnViaWM0IiwiYSI6ImNrY3Vla3R1ZjF0YnYyeXQ2c243eWVpeHEifQ.Hgj0BjhuuOAowR_pE97V_Q';
 
 const satStyle = 'mapbox://styles/rubic4/ckwql40zd202w15o358fdep6y';
-const darkStyle = 'mapbox://styles/rubic4/ckrkr81ox0l1x17o1i8jsfmnu';
-const lightStyle = 'mapbox://styles/rubic4/ckrkrcp3q9jrp17nyfgo2gzto';
 
 var map = new mapboxgl.Map({
     container: 'map',
     style: satStyle,
-    //center: [-122.486052, 37.830348],
-    //center: [-75.789, 41.874],
     center: [21.226788, 45.75],
     minZoom: 11,
     maxZoom: 17,
@@ -18,29 +14,6 @@ var map = new mapboxgl.Map({
 map.on('style.load', function() {
     map.on('click', function(e) {
     let coordinates = e.lngLat;
-    console.log(coordinates);
-    let theLat = coordinates.lat;
-    let theLon = coordinates.lng;
-    let linkToNames = "https://nominatim.openstreetmap.org/reverse.php?lat=" + theLat + "&" + "lon=" + theLon + "&format=jsonv2";
-    //console.log("AICI " + linkToNames);
-    //LAAT = theLat;
-    //LOON = theLon;
-
-    let linkNearbyTheatres = "https://overpass-api.de/api/interpreter?&data=[out:json];(node[%22amenity%22](around:1000," + theLat + "," + theLon + "););out%2050;";
-    //console.log("AICI 2 " + linkNearbyTheatres);
-    $.getJSON(linkNearbyTheatres, function(data) {
-        // JSON result in `data` variable
-        //console.log(data);
-        /*
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML('<img src="./Places/GaraDeNord/gara1.png">')
-            .addTo(map);  */
-        //displayMarkers(data);
-    });
-
-    
-
     });
 });
 
@@ -177,7 +150,7 @@ const Places = {
         },
     ]
 }
-//console.log(Places);
+
 function displayPlaces() {
     let len = Places.places.length;
 
@@ -189,14 +162,12 @@ function displayPlaces() {
 
         el.onclick = function(){images(i)}
 
-        //mapboxgl.Marker(el).setLngLat()
         // make a marker for each feature and add to the map
         new mapboxgl.Marker(el).setLngLat([Places.places[i].coord[1], Places.places[i].coord[0]]).addTo(map);
     }
 }
 
 function images(pos) {
-    //console.log(pos);
     var div = document.createElement('div');
     div.id = 'places';
     document.body.appendChild(div);
@@ -212,7 +183,6 @@ function images(pos) {
     div.appendChild(close);
 
     let len = Places.places[pos].photos.length;
-    //console.log("LEN " + len);
 
     for(let j = 0 ; j < len; j++) {
         var place = document.createElement('div');
@@ -226,7 +196,6 @@ function images(pos) {
 }
 
 function displayMarkers(data) {
-    //console.log(data.elements);
     let len = data.elements.length;
 
     for (let feature = 0; feature < len; feature++) {
@@ -235,7 +204,6 @@ function displayMarkers(data) {
         el.className = 'marker';
         el.textContent = data.elements[feature].tags.amenity;
 
-        //mapboxgl.Marker(el).setLngLat()
         // make a marker for each feature and add to the map
         new mapboxgl.Marker(el).setLngLat([data.elements[feature].lon, data.elements[feature].lat]).addTo(map);
     }
@@ -249,7 +217,6 @@ document.getElementById("slider").oninput = function() {
 
 function OpacityControl() {
     let value = document.getElementById("slider").value;
-    //console.log(value - '0');
     map.on('load', () => {
         map.addSource('radar', {
             'type': 'image',
@@ -264,10 +231,7 @@ function OpacityControl() {
         map.addLayer({
             id: 'radar-layer',
             'type': 'raster',
-            'source': 'radar'/*
-            'paint' : {
-                'raster-opacity': document.getElementById("slider").value - '0'
-            } */
+            'source': 'radar'
         });
 
         
@@ -300,10 +264,7 @@ function OpacityControl() {
         map.addLayer({
             id: 'radar-layer2',
             'type': 'raster',
-            'source': 'radar2'/*
-            'paint' : {
-                'raster-opacity': document.getElementById("slider").value - '0'
-            } */
+            'source': 'radar2'
         });
 
         
@@ -336,34 +297,10 @@ document.getElementById("showPlaces").onclick = function() {
         div.textContent = "Explore";
         stopDisplayPlaces();
     }
-    /*
-    var div = document.createElement('div');
-    div.id = 'places';
-    document.body.appendChild(div);
-
-    let len = Places.places.length;
-
-
-    for(let i = 0; i < len; i++) {
-        let lenn = Places.places[i].photos.length;
-        console.log(lenn);
-
-        for(let j = 0 ; j < lenn; j++) {
-            var place = document.createElement('div');
-            place.id = 'thePlace';
-
-            place.style.backgroundImage = "url('" + Places.places[i].photos[j] + "')";
-            
-
-            div.appendChild(place);
-        }
-    }   
-    */
 }
 
 function stopDisplayPlaces() {
    var places = document.getElementsByClassName('marker mapboxgl-marker mapboxgl-marker-anchor-center');
-   //console.log(places.length);
     let len = places.length;
 
    while(len) {
